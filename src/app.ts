7963a91d9ae9c05289bf9b00dd18ce490b076a7d
Uche Joseph <josephuche2404@gmail.com>
2026-06-17 20:20:43 +0300

    Register event routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import eventRoutes from "./modules/events/event.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/events", eventRoutes);
 
 export default app;
